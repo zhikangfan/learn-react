915497f4bd5b560271819dbb8d73040a2364c1fa
zhikangfan <zhikangfan@outlook.com>
2023-05-23 15:31:40 +0800

    feat: 学习使用redux以及react-router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@
 import './App.css';
 import FlyInto from './components/FlyInto'
 import {Test2} from "./components/test2";
+import {useDispatch, useSelector} from "react-redux";
+import {increment, decrement} from "./store/slice/counterSlice";
+import {Link, Route, Routes} from "react-router-dom";
+import Help from "./help";
+import Main from './Main';
+
 function App() {
+    const count = useSelector(state => state.counter.value)
+    const dispatch = useDispatch()
   return (
     <div className="App">
-      <FlyInto/>
-        <Test2/>
+        {count}
+          Hello world
+
+        <Link to={'/help'}>Help</Link>
+        <Link to={'/'}>Main</Link>
+        <Routes>
+            <Route path={"/"} element={<Main />}></Route>
+
+        </Routes>
+        <div style={{position: 'fixed', top: 20, left: 50, backgroundColor: 'red'}}>
+            <Routes>
+                <Route path={"/help"} element={<Help />}></Route>
+            </Routes>
+        </div>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,16 @@
-import {createStore} from 'redux'
-
-function counter(state = 0, action) {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
-}
-export let store = createStore(counter);
+// import {createStore} from 'redux'
+//
+// function counter(state = 0, action) {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// }
+// export let store = createStore(counter);
 
 
 // store.dispatch({ type: 'INCREMENT' });
@@ -19,3 +19,11 @@ export let store = createStore(counter);
 // // 2
 // store.dispatch({ type: 'DECREMENT' });
 // 1
+
+import {configureStore} from "@reduxjs/toolkit";
+import counterReducer from './slice/counterSlice'
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+})
